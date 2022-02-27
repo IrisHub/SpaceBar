e1a2da9b6540ae472975153d7e686f60b300c75b
Kanyes Thaker <krthaker1224@berkeley.edu>
2022-02-27 01:56:48 -0800

    Modify eslintrc.json and fix bug in movement logic

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -1,55 +1,55 @@
 import React, { useEffect, useRef } from 'react';
 
 export default function Instructions(props: { controls: any; }){
-    const { controls } = props;
-    const blockerRef = useRef<HTMLDivElement>(null);
-    const instructionsRef = useRef<HTMLDivElement>(null);
+  const { controls } = props;
+  const blockerRef = useRef<HTMLDivElement>(null);
+  const instructionsRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (instructionsRef.current) {
-            instructionsRef.current.addEventListener('click', () => { controls.lock(); });
-        }
-    }, [controls]);
-    useEffect(() => {
-        controls.addEventListener('lock', () => {
-            if (instructionsRef.current && blockerRef.current){
-                instructionsRef.current.style.display = 'none';
-                blockerRef.current.style.display = 'none';
-            }
-        });
-    }, [controls]);
-    useEffect(() => {
-        controls.addEventListener('unlock', () => {
-            if (instructionsRef.current && blockerRef.current){
-                blockerRef.current.style.display = 'block';
-                instructionsRef.current.style.display = 'flex';
-            }
-        });
-    }, [controls]);
+  useEffect(() => {
+    if (instructionsRef.current) {
+      instructionsRef.current.addEventListener('click', () => { controls.lock(); });
+    }
+  }, [controls]);
+  useEffect(() => {
+    controls.addEventListener('lock', () => {
+      if (instructionsRef.current && blockerRef.current){
+        instructionsRef.current.style.display = 'none';
+        blockerRef.current.style.display = 'none';
+      }
+    });
+  }, [controls]);
+  useEffect(() => {
+    controls.addEventListener('unlock', () => {
+      if (instructionsRef.current && blockerRef.current){
+        blockerRef.current.style.display = 'block';
+        instructionsRef.current.style.display = 'flex';
+      }
+    });
+  }, [controls]);
 
-    return (
+  return (
         <div
             style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(0,0,0,0.5)',
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0,0,0,0.5)',
             }}
             ref={blockerRef}
         >
             <div
             style={{
-                width: '100%',
-                height: '100%',
+              width: '100%',
+              height: '100%',
     
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
     
-                textAlign: 'center',
-                fontSize: '36px',
-                cursor: 'pointer',
+              textAlign: 'center',
+              fontSize: '36px',
+              cursor: 'pointer',
             }}
             ref={instructionsRef}
             >
@@ -63,5 +63,5 @@ export default function Instructions(props: { controls: any; }){
             </p>
             </div>
         </div>
-        );
+  );
 }
--- src/controls.ts
+++ src/controls.ts
@@ -1,76 +1,84 @@
 export class Controller {
-    forward: boolean;
-    backward: boolean;
-    left: boolean;
-    right: boolean;
-    zVector: { direction: number; magnitude: number; };
-    xVector: { direction: number; magnitude: number; };
-    time: number;
-    controls: { isLocked: boolean; moveRight: any; moveForward: any; };
-    constructor(controls: { isLocked: boolean, moveRight: any, moveForward: any, }) {
+  forward: boolean;
+
+  backward: boolean;
+
+  left: boolean;
+
+  right: boolean;
+
+  zVector: { direction: number; magnitude: number; };
+
+  xVector: { direction: number; magnitude: number; };
+
+  time: number;
+
+  controls: { isLocked: boolean; moveRight: any; moveForward: any; };
+
+  constructor(controls: { isLocked: boolean, moveRight: any, moveForward: any, }) {
+    this.forward = false;
+    this.backward = false;
+    this.left = false;
+    this.right = false;
+    this.zVector = { direction: 0, magnitude: 0 };
+    this.xVector = { direction: 0, magnitude: 0 };
+    this.time = performance.now();
+    this.controls = controls;
+  }
+
+  onKeyDown = ( event: { key: string }) => {
+    switch (event.key) {
+      case 'w':
+        this.forward = true;
+        break;
+      case 'a':
+        this.left = true;
+        break;
+      case 's':
+        this.backward = true;
+        break;
+      case 'd':
+        this.right = true;
+        break;
+      default:
+        break;
+    }
+  };
+
+  onKeyUp = ( event: { key: string }) => {
+    switch (event.key) {
+      case 'w':
         this.forward = false;
-        this.backward = false;
+        break;
+      case 'a':
         this.left = false;
+        break;
+      case 's':
+        this.backward = false;
+        break;
+      case 'd':
         this.right = false;
-        this.zVector = { direction: 0, magnitude: 0};
-        this.xVector = { direction: 0, magnitude: 0};
-        this.time = performance.now();
-        this.controls = controls;
+        break;
+      default:
+        break;
     }
+  };
 
-    onKeyDown = ( event: { key: string }) => {
-        switch (event.key) {
-            case 'w':
-                this.forward = true;
-                break;
-            case 'a':
-                this.left = true;
-                break;
-            case 's':
-                this.backward = true;
-                break;
-            case 'd':
-                this.right = true;
-                break;
-            default:
-                break;
-        }
-    };
-
-    onKeyUp = ( event: { key: string }) => {
-        switch (event.key) {
-            case 'w':
-                this.forward = false;
-                break;
-            case 'a':
-                this.left = false;
-                break;
-            case 's':
-                this.backward = false;
-                break;
-            case 'd':
-                this.right = false;
-                break;
-            default:
-                break;
-        }
-    };
-
-    move = ( newTime: number, speed: number ) => {
-        if (this.controls.isLocked === true) {
-            const timeDelta = (newTime - this.time) / 1000;
-            this.zVector.direction = Number(this.forward) - Number(this.backward);
-            this.xVector.direction = Number(this.right) - Number(this.left);
-            this.xVector.magnitude = 0; this.zVector.magnitude = 0;
-            if (this.forward || this.backward) {
-                this.zVector.magnitude = this.zVector.direction * speed * timeDelta;
-            }
-            if (this.right || this.left) {
-                this.zVector.magnitude = this.zVector.direction * speed * timeDelta;
-            }
-            this.controls.moveRight(this.xVector.magnitude * timeDelta);
-            this.controls.moveForward(this.zVector.magnitude * timeDelta);
-        }
-        this.time = newTime;
+  move = ( newTime: number, speed: number ) => {
+    if (this.controls.isLocked === true) {
+      const timeDelta = (newTime - this.time) / 1000;
+      this.zVector.direction = Number(this.forward) - Number(this.backward);
+      this.xVector.direction = Number(this.right) - Number(this.left);
+      this.xVector.magnitude = 0; this.zVector.magnitude = 0;
+      if (this.forward || this.backward) {
+        this.zVector.magnitude = this.zVector.direction * speed * timeDelta;
+      }
+      if (this.right || this.left) {
+        this.xVector.magnitude = this.xVector.direction * speed * timeDelta;
+      }
+      this.controls.moveRight(this.xVector.magnitude * timeDelta);
+      this.controls.moveForward(this.zVector.magnitude * timeDelta);
     }
+    this.time = newTime;
+  };
 }
