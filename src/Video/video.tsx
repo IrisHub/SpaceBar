266e99ff008070c46a81cb37533dab84a57198cf
samjgorman <samjgorman@gmail.com>
2022-04-17 23:34:09 -0700

    Video: toggle local video & audio on / off

--- src/Video/video.tsx
+++ src/Video/video.tsx
@@ -1,6 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
-import './video.css';
+import styled from 'styled-components';
+
+const VideoContainer = styled.div`
+  position: absolute;
+  z-index: 999;
+`;
+
+const AudioToggle = styled.button``;
+const VideoToggle = styled.button``;
+
 function VideoPlayer() {
+  const [audioOn, setAudioOn] = useState(true);
+  const [videoOn, setVideoOn] = useState(true);
+
   const permissions = {
     video: true,
     audio: true,
@@ -20,14 +32,45 @@ function VideoPlayer() {
     getMedia();
   }, []);
 
+  function toggleVideoAudio(trackKind: string) {
+    if (videoRef.current?.srcObject) {
+      let stream = videoRef.current.srcObject as MediaStream;
+      stream.getTracks().forEach(function (track) {
+        if (track.readyState == 'live' && track.kind === trackKind) {
+          // track.stop(); Permanent
+          track.enabled = !track.enabled;
+        }
+      });
+    } else {
+      console.error('No valid src object present');
+    }
+  }
+
+  function handleVideo() {
+    console.log('video fired');
+    setVideoOn(!videoOn);
+    toggleVideoAudio('video');
+  }
+
+  function handleAudio() {
+    console.log('audio fired');
+    setAudioOn(!audioOn);
+    toggleVideoAudio('audio');
+  }
+
   return (
-    <video
-      ref={videoRef}
-      className="vid"
-      height="120"
-      width="160"
-      autoPlay
-    ></video>
+    <VideoContainer>
+      <video
+        ref={videoRef}
+        className="vid"
+        height="120"
+        width="160"
+        autoPlay
+      ></video>
+
+      <AudioToggle onClick={handleAudio}>Audio </AudioToggle>
+      <VideoToggle onClick={handleVideo}> Video</VideoToggle>
+    </VideoContainer>
   );
 }
 
